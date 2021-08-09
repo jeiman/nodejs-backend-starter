e411e1f6760dec3fbe038bd8c1f6d512f8400546
Jeiman Jeya <johnfaith7@gmail.com>
2021-08-09 23:08:42 +0800

    adding duplicated lines to trigger QG check

--- src/schemas.js
+++ src/schemas.js
@@ -31,8 +31,24 @@ module.exports = (db) => {
       )
   `;;
 
+  const createRideTableSchema3 = `
+      CREATE TABLE Rides3
+      (
+      rideID INTEGER PRIMARY KEY AUTOINCREMENT,
+      startLat DECIMAL NOT NULL,
+      startLong DECIMAL NOT NULL,
+      endLat DECIMAL NOT NULL,
+      endLong DECIMAL NOT NULL,
+      riderName TEXT NOT NULL,
+      driverName TEXT NOT NULL,
+      driverVehicle TEXT NOT NULL,
+      created DATETIME default CURRENT_TIMESTAMP
+      )
+  `;;
+
   db.run(createRideTableSchema);
   db.run(createRideTableSchema2);
+  db.run(createRideTableSchema3);
 
   return db;
 };
