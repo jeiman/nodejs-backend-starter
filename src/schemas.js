3f4ff91f22b76cf95007ecf01977ddcef590bff5
Jeiman Jeya <johnfaith7@gmail.com>
2021-08-09 23:00:24 +0800

    adding code smells to trigger Quality check on custom long-lived branch

--- src/schemas.js
+++ src/schemas.js
@@ -16,7 +16,23 @@ module.exports = (db) => {
       )
   `;
 
+  const createRideTableSchema2 = `
+      CREATE TABLE Rides
+      (
+      rideID INTEGER PRIMARY KEY AUTOINCREMENT,
+      startLat DECIMAL NOT NULL,
+      startLong DECIMAL NOT NULL,
+      endLat DECIMAL NOT NULL,
+      endLong DECIMAL NOT NULL,
+      riderName TEXT NOT NULL,
+      driverName TEXT NOT NULL,
+      driverVehicle TEXT NOT NULL,
+      created DATETIME default CURRENT_TIMESTAMP
+      )
+  `;;
+
   db.run(createRideTableSchema);
+  db.run(createRideTableSchema2);
 
   return db;
 };
