71b9082ec2e324b97a2bc5d9eeaa08c390a824af
Jeiman Jeya <johnfaith7@gmail.com>
2021-08-10 10:23:40 +0800

    Testing code smells on Sonar Cloud Analysis

--- src/schemas.js
+++ src/schemas.js
@@ -16,6 +16,38 @@ module.exports = (db) => {
       )
   `;
 
+  const createRideTableSchema2 = `
+      CREATE TABLE Rides2
+      (
+      rideID INTEGER PRIMARY KEY AUTOINCREMENT,
+      startLat DECIMAL NOT NULL,
+      startLong DECIMAL NOT NULL,
+      endLat DECIMAL NOT NULL,
+      endLong DECIMAL NOT NULL,
+      riderName TEXT NOT NULL,
+      driverName TEXT NOT NULL,
+      driverVehicle TEXT NOT NULL,
+      created DATETIME default CURRENT_TIMESTAMP
+      )
+  `;;
+
+  const createRideTableSchema3 = `
+      CREATE TABLE Rides3
+      (
+      rideID INTEGER PRIMARY KEY AUTOINCREMENT,
+      startLat DECIMAL NOT NULL,
+      startLong DECIMAL NOT NULL,
+      endLat DECIMAL NOT NULL,
+      endLong DECIMAL NOT NULL,
+      riderName TEXT NOT NULL,
+      driverName TEXT NOT NULL,
+      driverVehicle TEXT NOT NULL,
+      created DATETIME default CURRENT_TIMESTAMP
+      )
+  `;;
+
   db.run(createRideTableSchema);
+  db.run(createRideTableSchema2);
+  db.run(createRideTableSchema3);
   return db;
 };
